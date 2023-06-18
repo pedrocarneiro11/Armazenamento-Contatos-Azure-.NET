8bf246f5cb8d2ce0dafc5ecaa20fdd3be1378bbe
Pedro Carneiro <pedrocarneirocunha87@gmail.com>
2023-06-18 12:00:59 -0300

    Métodos GET, POST e PUT

--- Controllers/ContatoController.cs
+++ Controllers/ContatoController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using _2_azure_table.Models;
+using Azure.Data.Tables;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -21,6 +23,40 @@ namespace _2_azure_table.Controllers
             _tableName = configuration.GetValue<string>("AzureTableName");
         }
 
-        
+        private TableClient GetTableClient()
+        {
+            var serviceClient = new TableServiceClient(_connectionString);
+            var tableClient = serviceClient.GetTableClient(_tableName);
+
+            tableClient.CreateIfNotExists();
+            return tableClient;
+        }
+
+        [HttpPost]
+        public IActionResult Criar(Contato contato)
+        {
+            var tableClient = GetTableClient();
+
+            contato.RowKey = Guid.NewGuid().ToString();
+            contato.PartitionKey = contato.RowKey;
+
+            tableClient.UpsertEntity(contato);
+            
+            return Ok(contato);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Atualizar(string id, Contato contato)
+        {
+            var tableClient = GetTableClient();
+            var contatoTable = tableClient.GetEntity<Contato>(id, id).Value;
+
+            contatoTable.Nome = contato.Nome;
+            contatoTable.Email = contato.Email;
+            contatoTable.Telefone = contato.Telefone;
+
+            tableClient.UpsertEntity(contatoTable);
+            return Ok();
+        }
     }
 }
